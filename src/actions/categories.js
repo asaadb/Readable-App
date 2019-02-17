2e99b339d86d6c860f7a34ab08ef9529e8cdcf61
Asaad Budayr <asad_289@hotmail.com>
2019-02-17 13:25:08 -0800

    Feat: Create categories actions file

---
+++ src/actions/categories.js
@@ -0,0 +1,17 @@
+import * as API from "../utils/Api";
+
+export const GET_CATEGORIES = "GET_CATEGORIES";
+
+export function receiveCategories(categories) {
+  return {
+    type: GET_CATEGORIES,
+    categories
+  };
+}
+export function handleReceiveCategories(categories) {
+  return dispatch => {
+    return API.getCategories().then(categories => {
+      dispatch(receiveCategories(categories));
+    });
+  };
+}
