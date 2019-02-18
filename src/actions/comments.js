e3d0696b6dbfe0b1a02a5f92ccda9f4b3838d141
Asaad Budayr <asad_289@hotmail.com>
2019-02-18 08:41:13 -0800

    Feat: Create comments actions file

---
+++ src/actions/comments.js
@@ -0,0 +1,89 @@
+import * as API from "../utils/Api";
+import uuid from "uuid";
+
+export const GET_COMMENTS = "GET_COMMENTS";
+export const ADD_COMMENT = "ADD_COMMENT";
+export const EDIT_COMMENT = "EDIT_COMMENT";
+export const DELETE_COMMENT = "DELETE_COMMENT";
+export const VOTE_COMMENT = "VOTE_COMMENT";
+
+export function receiveComments(comments) {
+  return {
+    type: GET_COMMENTS,
+    comments
+  };
+}
+
+export function createComment(comment) {
+  return {
+    type: ADD_COMMENT,
+    comment
+  };
+}
+
+export function updateComment(comment) {
+  return {
+    type: EDIT_COMMENT,
+    comment
+  };
+}
+
+export function voteComment({ id, option }) {
+  return {
+    type: VOTE_COMMENT,
+    id,
+    option
+  };
+}
+
+export function removeComment(id) {
+  return {
+    type: DELETE_COMMENT,
+    id
+  };
+}
+
+export function handleReceiveComments(id) {
+  return dispatch => {
+    return API.getCommentsForPost(id).then(comments => {
+      dispatch(receiveComments(comments));
+    });
+  };
+}
+
+export function handleCreateComments({ body, author, parentId }) {
+  let id = uuid();
+  let timestamp = Date.now();
+  return dispatch => {
+    return API.addComment({ body, author, parentId, id, timestamp }).then(
+      comment => {
+        dispatch(createComment(comment));
+      }
+    );
+  };
+}
+
+export function handleEditComment({ id, body }) {
+  let timestamp = Date.now();
+  return dispatch => {
+    return API.editComment({ id, body, timestamp }).then(() => {
+      dispatch(updateComment({ id, timestamp, body }));
+    });
+  };
+}
+
+export function handleVoteComment({ id, option }) {
+  return dispatch => {
+    return API.voteOnComment({ id, option }).then(() => {
+      dispatch(voteComment({ id, option }));
+    });
+  };
+}
+
+export function handleDeleteComment(id) {
+  return dispatch => {
+    return API.deleteComment(id).then(() => {
+      dispatch(removeComment(id));
+    });
+  };
+}
