8f3e95898247719ed3cf00ef512bc0f905827912
Asaad Budayr <asad_289@hotmail.com>
2019-02-24 09:36:52 -0800

    Feat: Move the Post styling to the Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,36 +6,72 @@ import {
   Card,
   CardContent,
   Typography,
-  CardActions
+  CardActions,
+  withStyles
 } from "@material-ui/core";
-import {
-  ThumbsUpDown,
-  Comment,
-  AccessTime,
-} from "@material-ui/icons";
-import PostActions from './PostActions'
+import { ThumbsUpDown, Comment, AccessTime } from "@material-ui/icons";
+import PostActions from "./PostActions";
+
+const styles = {
+  post: {
+    width: 450,
+    margin: "1rem",
+    display: "flex"
+  },
+  content: {
+    width: "95%",
+    display: "flex",
+    flexFlow: "column",
+    boxSizing: "border-box",
+    margin: 10
+  },
+  title: {
+    fontSize: "2rem"
+  },
+  author: {
+    color: "#A9A9A9",
+    fontSize: "1rem",
+    margin: "10px 0"
+  },
+  voteBody: {
+    display: "flex",
+    width: "100%",
+    boxSizing: "border-box",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "0.5rem"
+  },
+  voteCount: {
+    display: "flex",
+    flexFlow: "row nowrap",
+    alignItems: "center",
+    margin: "1rem"
+  },
+  postCount: {
+    marginLeft: 10,
+    display: "flex",
+    flexFlow: "row nowrap"
+  }
+};
 
 class Post extends Component {
-
   render() {
-    const { post } = this.props;
+    const { post, classes } = this.props;
     return (
-      <Card key={post.id} className="post">
-        <CardContent>
+      <Card key={post.id} className={classes.post}>
+        <CardContent className={classes.content}>
           <Typography variant="h6" noWrap type="title">
             {post.title}
           </Typography>
-          <div className="author">
+          <div className={classes.author}>
             <AccessTime style={{ marginRight: 5 }} />
             <span>
               {post.author} posted {formatDate(post.timestamp)}
             </span>
           </div>
-          <div className="vote-body">
-            <Typography noWrap className="post-body">
-              {post.body}
-            </Typography>
-            <div className="vote-count">
+          <div className={classes.voteBody}>
+            <Typography noWrap>{post.body}</Typography>
+            <div className={classes.postCount}>
               <Badge
                 badgeContent={post.voteScore}
                 style={{ marginRight: 20 }}
@@ -50,7 +86,7 @@ class Post extends Component {
             </div>
           </div>
           <CardActions>
-          <PostActions showLink={true} post={post} />
+            <PostActions showLink={true} post={post} />
           </CardActions>
         </CardContent>
       </Card>
@@ -58,4 +94,4 @@ class Post extends Component {
   }
 }
 
-export default connect()(Post);
+export default connect()(withStyles(styles)(Post));
