7254ce59ec546550b9ce7c31540639b380ed6ad7
Asaad Budayr <asad_289@hotmail.com>
2019-02-24 10:58:55 -0800

    Feat: Create CommentActions to handle actions

---
+++ src/components/CommentActions.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { IconButton } from "@material-ui/core";
+import { ThumbUp, ThumbDown, Edit, Delete } from "@material-ui/icons";
+import { handleVoteComment, handleDeleteComment } from "../actions/comments";
+
+const styles = {
+  container: {
+    display: "flex",
+    flexFlow: "row wrap",
+    justifyContent: "space-between",
+    width: "100%"
+  }
+};
+
+class CommentActions extends Component {
+  handleVote = option => {
+    const { dispatch } = this.props;
+    const { id } = this.props.comment;
+    dispatch(handleVoteComment({ id, option }));
+  };
+  handleRemove = () => {
+    const { dispatch } = this.props;
+    const { id } = this.props.comment;
+    dispatch(handleDeleteComment(id));
+  };
+  render() {
+    return (
+      <div style={styles.container}>
+        <div>
+          <IconButton color="primary" aria-label="open">
+            <Edit />
+          </IconButton>
+          <IconButton
+            color="secondary"
+            aria-label="open"
+            onClick={this.handleRemove}
+          >
+            <Delete />
+          </IconButton>
+        </div>
+        <div>
+          <IconButton
+            color="primary"
+            aria-label="open"
+            onClick={() => this.handleVote("upVote")}
+          >
+            <ThumbUp />
+          </IconButton>
+          <IconButton
+            color="secondary"
+            aria-label="open"
+            onClick={() => this.handleVote("downVote")}
+          >
+            <ThumbDown />
+          </IconButton>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect()(CommentActions);
--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,79 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { formatDate } from "../utils/helpers";
 import {
   Badge,
-  IconButton,
   Card,
   CardActions,
   CardContent,
   Typography
 } from "@material-ui/core";
-import {
-  ThumbsUpDown,
-  ThumbUp,
-  ThumbDown,
-  Edit,
-  Delete
-} from "@material-ui/icons";
+import { ThumbsUpDown } from "@material-ui/icons";
+import CommentActions from "./CommentActions";
 
 class Comments extends Component {
-  handleVote = (option) => {
-    console.log(option)
-  }
   render() {
-    const { comment } = this.props
-    console.log('comments inside Comment', comment);
-  return (
-    <Card key={comment.id} className="comment">
-      <CardContent>
-        <div className="author">
-          <span>
-            {comment.author} commented {formatDate(comment.timestamp)}
-          </span>
-        </div>
-        <div className="vote-body">
-          <Typography noWrap className="comment-body">
-            {comment.body}
-          </Typography>
-          <div className="vote-count">
-            <Badge
-              badgeContent={comment.voteScore}
-              style={{ marginRight: 20 }}
-              max={999}
-              color="primary"
-            >
-              <ThumbsUpDown />
-            </Badge>
+    const { comment } = this.props;
+    return (
+      <Card key={comment.id} className="comment">
+        <CardContent>
+          <div className="author">
+            <span>
+              {comment.author} commented {formatDate(comment.timestamp)}
+            </span>
           </div>
-        </div>
-        <CardActions className="action-icons">
-          <IconButton color="primary" aria-label="open">
-            <Edit />
-          </IconButton>
-          <IconButton color="secondary" aria-label="open">
-            <Delete />
-          </IconButton>
-          <IconButton
-            color="primary"
-            aria-label="open"
-            onClick={() => this.handleVote("upVote")}
-          >
-            <ThumbUp />
-          </IconButton>
-          <IconButton
-            color="secondary"
-            aria-label="open"
-            onClick={() => this.handleVote("downVote")}
-          >
-            <ThumbDown />
-          </IconButton>
-        </CardActions>
-      </CardContent>
-    </Card>
-  )
-}
+          <div className="vote-body">
+            <Typography noWrap className="comment-body">
+              {comment.body}
+            </Typography>
+            <div className="vote-count">
+              <Badge
+                badgeContent={comment.voteScore}
+                style={{ marginRight: 20 }}
+                max={999}
+                color="primary"
+              >
+                <ThumbsUpDown />
+              </Badge>
+            </div>
+          </div>
+          <CardActions>
+            <CommentActions comment={comment} />
+          </CardActions>
+        </CardContent>
+      </Card>
+    );
+  }
 }
 
-
-export default connect()(Comments)
+export default connect()(Comments);
