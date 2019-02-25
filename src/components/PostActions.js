c78e473bc29acedb56aecd1df62d4f0bc6bdaf52
Asaad Budayr <asad_289@hotmail.com>
2019-02-25 13:52:28 -0800

    Feat: Add edit post functionality
    Create a dialog to allow users to edit posts

--- src/components/PostActions.js
+++ src/components/PostActions.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { IconButton } from "@material-ui/core";
+import { IconButton,   Button,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle } from "@material-ui/core";
 import {
   ThumbUp,
   ThumbDown,
   OpenInNew,
   Edit,
-  Delete
+  Delete,
 } from "@material-ui/icons";
-import { handleVotePost, handleRemovePost } from "../actions/posts";
+import { handleVotePost, handleRemovePost, handleUpdatePost } from "../actions/posts";
 
 const styles = {
   container: {
@@ -21,6 +26,45 @@ const styles = {
 };
 
 class PostActions extends Component {
+  state = {
+    title:this.props.post.title,
+    body: this.props.post.body,
+    open:false,
+
+  }
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({
+       open: false,
+       title:this.props.post.title,
+       body: this.props.post.body,
+      });
+  };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+  isDisabled = () => {
+    const { body, title } = this.state;
+    return body === "" || title === "";
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    const { dispatch, post } = this.props;
+    const { title, body } = this.state;
+    const { id } = post
+    dispatch(handleUpdatePost({id, title, body}))
+    this.setState({
+      open: false,
+      body: "",
+      title: ""
+    });
+  };
   handleVote = option => {
     const { dispatch } = this.props;
     const { id } = this.props.post;
@@ -33,6 +77,7 @@ class PostActions extends Component {
   };
   render() {
     const { post, showLink } = this.props;
+    const { title, body } = this.state
     return (
       <div style={styles.container}>
         <div>
@@ -43,7 +88,7 @@ class PostActions extends Component {
               </Link>
             </IconButton>
           )}
-          <IconButton color="primary" aria-label="open">
+          <IconButton color="primary" aria-label="open" onClick={this.handleClickOpen}>
             <Edit />
           </IconButton>
           <IconButton
@@ -70,6 +115,53 @@ class PostActions extends Component {
             <ThumbDown />
           </IconButton>
         </div>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Edit Post</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              required
+              variant="outlined"
+              margin="dense"
+              id="name"
+              label="title"
+              type="title"
+              fullWidth
+              value={title}
+              onChange={this.handleChange("title")}
+            />
+            <TextField
+              required
+              variant="outlined"
+              margin="dense"
+              id="body"
+              label="Body"
+              type="text"
+              multiline={true}
+              rows={5}
+              fullWidth
+              value={body}
+              onChange={this.handleChange("body")}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button
+              disabled={this.isDisabled()}
+              onClick={this.handleSubmit}
+              type="submit"
+              color="primary"
+            >
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }
