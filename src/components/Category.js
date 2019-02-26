a1d080d8b58d7069ff7d78e5bbc4cf1ab1c690c8
Asaad Budayr <asad_289@hotmail.com>
2019-02-26 11:54:28 -0800

    Feat: Improve the styling of Category

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import Post from "./Post";
+import { withStyles } from "@material-ui/core";
+
+const styles = {
+  container: {
+    width: "100%",
+    maxWidth: 1500,
+    marginLeft: "auto",
+    marginRight: "auto"
+  },
+  noPost: {
+    textAlign: "center",
+    fontSize: "1rem"
+  }
+};
 
 class Category extends Component {
   render() {
-    const { categoryPosts } = this.props
+    const { categoryPosts, classes, category } = this.props;
     return (
-      <div>
-        <ul>
+      <div className={classes.container}>
+        <h3 className="section-header"> {category} </h3>
+        {categoryPosts.length === 0 && (
+          <p className={classes.noPost}>
+            There are no posts related to {category}
+          </p>
+        )}
+        <ul className="posts">
           {categoryPosts.map(post => (
-            <Post post={post} key={post.id} showLink={true}/>
-          ))
-          }
+            <Post post={post} key={post.id} showLink={true} />
+          ))}
         </ul>
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps( { posts },  ownProps) {
-  const { category } =  ownProps.match.params
-  const categoryPosts = posts.filter(post => post.category === category)
+function mapStateToProps({ posts }, ownProps) {
+  const { category } = ownProps.match.params;
+  const categoryPosts = posts.filter(post => post.category === category);
   return {
-    categoryPosts,
+    category,
+    categoryPosts
   };
 }
-export default connect(mapStateToProps)(Category);
+
+export default connect(mapStateToProps)(withStyles(styles)(Category));
